25ee2a6ab8f5b427ff5134557d82a46ffcfeabc8
josray10 <jossyraya.saragih@gmail.com>
2025-02-02 03:51:04 +0700

    Change Default DataPenghuni Page to DataMahasiswa while open

--- src/app/admin/datapenghuni/page.jsx
+++ src/app/admin/datapenghuni/page.jsx
@@ -1,22 +1,21 @@
-"use client";
-import { useState } from "react";
-import { useRouter } from "next/navigation";  // Import useRouter
-import Select from "@/components/elements/Select";
-import DataKasra from "./datakasra"; // Import halaman Data Kasra
-import DataMahasiswa from "./datamahasiswa";
+'use client';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation'; // Import useRouter
+import Select from '@/components/elements/Select';
+import DataKasra from './datakasra'; // Import halaman Data Kasra
+import DataMahasiswa from './datamahasiswa';
 
 const KelolaData = () => {
-  const [categories, setCategories] = useState("DEFAULT");
+  const [categories, setCategories] = useState('Data Mahasiswa');
 
   const valueOption = [
-    { value: "DEFAULT", label: "Pilih Kategori", disabled: "disabled" },
-    { value: "Data Kasra", label: "Data Kasra", disabled: "" },
-    { value: "Data Mahasiswa", label: "Data Mahasiswa", disabled: "" },
+    { value: 'DEFAULT', label: 'Pilih Kategori', disabled: 'disabled' },
+    { value: 'Data Kasra', label: 'Data Kasra', disabled: '' },
+    { value: 'Data Mahasiswa', label: 'Data Mahasiswa', disabled: '' },
   ];
 
   return (
     <div>
-
       {/* Dropdown Pilihan */}
       <div className="w-full p-5">
         <Select
@@ -28,7 +27,7 @@ const KelolaData = () => {
       </div>
 
       {/* Konten yang dipilih */}
-      {categories === "Data Mahasiswa" ? <DataMahasiswa /> : <DataKasra />}
+      {categories === 'Data Mahasiswa' ? <DataMahasiswa /> : <DataKasra />}
     </div>
   );
 };
