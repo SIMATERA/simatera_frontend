c50447a1e8271a12d09cbe5a7d5bb3e67c4b17d6
josray10 <jossyraya.saragih@gmail.com>
2025-02-02 03:43:22 +0700

    Make menu file for better sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,14 +1,14 @@
-"use client";
-import React from "react";
-import { 
-  MENU_ITEMS_ADMIN, 
-  MENU_ITEMS_KASRA, 
-  MENU_ITEMS_MAHASISWA 
-} from "@/constant/menu";
+'use client';
+import React from 'react';
+import {
+  MENU_ITEMS_ADMIN,
+  MENU_ITEMS_KASRA,
+  MENU_ITEMS_MAHASISWA,
+} from '@/constant/menu';
 
 const Sidebar = ({ role, activeMenu, setActiveMenu }) => {
   const getMenuItems = () => {
-    switch(role) {
+    switch (role) {
       case 'admin':
         return MENU_ITEMS_ADMIN;
       case 'kasra':
@@ -23,10 +23,10 @@ const Sidebar = ({ role, activeMenu, setActiveMenu }) => {
   return (
     <div className="w-64 bg-gray-800 text-white p-5 flex flex-col space-y-4 h-full">
       <div className="flex flex-col items-center mb-6">
-        <img 
-          src="/images/logoasrama.png" 
-          alt="Logo Asrama ITERA" 
-          className="w-16 h-16 mb-2" 
+        <img
+          src="/images/logoasrama.png"
+          alt="Logo Asrama ITERA"
+          className="w-16 h-16 mb-2"
         />
         <h2 className="text-xl font-bold text-center">
           Dashboard <br /> SIMATERA
@@ -38,7 +38,9 @@ const Sidebar = ({ role, activeMenu, setActiveMenu }) => {
             <button
               onClick={() => setActiveMenu(item.key)}
               className={`w-full flex items-center gap-2 text-left p-3 rounded-md ${
-                activeMenu === item.key ? "bg-orange-700" : "hover:bg-orange-700"
+                activeMenu === item.key
+                  ? 'bg-orange-700'
+                  : 'hover:bg-orange-700'
               }`}
             >
               {item.icon} {item.name}
