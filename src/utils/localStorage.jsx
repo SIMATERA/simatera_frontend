d7a9f65dfabe398b9ca3bc7e9892035b6c5f446c
josray10 <jossyraya.saragih@gmail.com>
2025-02-02 03:42:18 +0700

    Make local storage for DataPelanggaran

--- src/utils/localStorage.jsx
+++ src/utils/localStorage.jsx
@@ -1,40 +1,59 @@
 export const getDataMahasiswa = () => {
-    if (typeof window !== 'undefined') {
-      const data = localStorage.getItem('mahasiswaData');
-      return data ? JSON.parse(data) : [];
-    }
-    return [];
-  };
-  
-export const saveDataMahasiswa = (data) => {
-    if (typeof window !== 'undefined') {
-      localStorage.setItem('mahasiswaData', JSON.stringify(data));
-    }
-  };
-  
-export const clearDataMahasiswa = () => {
-    if (typeof window !== 'undefined') {
-      localStorage.removeItem('mahasiswaData');
-    }
-  };
+  if (typeof window !== 'undefined') {
+    const data = localStorage.getItem('mahasiswaData');
+    return data ? JSON.parse(data) : [];
+  }
+  return [];
+};
 
-  // localStorage.js
+export const saveDataMahasiswa = (data) => {
+  if (typeof window !== 'undefined') {
+    localStorage.setItem('mahasiswaData', JSON.stringify(data));
+  }
+};
+
+export const clearDataMahasiswa = () => {
+  if (typeof window !== 'undefined') {
+    localStorage.removeItem('mahasiswaData');
+  }
+};
+
 export const getDataKasra = () => {
-    if (typeof window !== 'undefined') {
-      const data = localStorage.getItem('kasraData');
-      return data ? JSON.parse(data) : [];
-    }
-    return [];
-  };
-  
+  if (typeof window !== 'undefined') {
+    const data = localStorage.getItem('kasraData');
+    return data ? JSON.parse(data) : [];
+  }
+  return [];
+};
+
 export const saveDataKasra = (data) => {
-    if (typeof window !== 'undefined') {
-      localStorage.setItem('kasraData', JSON.stringify(data));
-    }
-  };
-  
+  if (typeof window !== 'undefined') {
+    localStorage.setItem('kasraData', JSON.stringify(data));
+  }
+};
+
 export const clearDataKasra = () => {
-    if (typeof window !== 'undefined') {
-      localStorage.removeItem('kasraData');
-    }
-  };
+  if (typeof window !== 'undefined') {
+    localStorage.removeItem('kasraData');
+  }
+};
+
+export const getDataPelanggaran = () => {
+  if (typeof window !== 'undefined') {
+    const data = localStorage.getItem('pelanggaranData');
+    return data ? JSON.parse(data) : [];
+  }
+  return [];
+};
+
+export const saveDataPelanggaran = (data) => {
+  if (typeof window !== 'undefined') {
+    localStorage.setItem('pelanggaranData', JSON.stringify(data));
+  }
+};
+
+export const clearDataPelanggaran = () => {
+  if (typeof window !== 'undefined') {
+    localStorage.removeItem('pelanggaranData');
+  }
+};
