ed0cd122eefbe0d18309958ae1a69ff7a7d2c7a7
josray10 <jossyraya.saragih@gmail.com>
2025-02-14 05:43:46 +0700

    feat(menu): standardize key values in menu items for consistency

--- src/constant/menu.jsx
+++ src/constant/menu.jsx
@@ -39,44 +39,44 @@ export const MENU_ITEMS_ADMIN = [
 ];
 
 export const MENU_ITEMS_KASRA = [
-  { name: 'Beranda', key: 'Beranda', icon: <Home size={20} /> },
-  { name: 'Data Penghuni', key: 'Data Penghuni', icon: <Users size={20} /> },
+  { name: 'Beranda', key: '', icon: <Home size={20} /> },
+  { name: 'Data Penghuni', key: 'datapenghuni', icon: <Users size={20} /> },
   {
     name: 'Data Pelanggaran',
-    key: 'Data Pelanggaran',
+    key: 'datapelanggaran',
     icon: <FileX size={20} />,
   },
   {
     name: 'Data Pembayaran',
-    key: 'Data Pembayaran',
+    key: 'datapembayaran',
     icon: <CreditCard size={20} />,
   },
   { name: 'Pengaduan', key: 'pengaduan', icon: <Megaphone size={20} /> },
   {
     name: 'Jadwal Kegiatan',
-    key: 'Jadwal Kegiatan',
+    key: 'jadwalkegiatan',
     icon: <CalendarClock size={20} />,
   },
-  { name: 'Pengaturan', key: 'Pengaturan', icon: <Settings size={20} /> },
+  { name: 'Pengaturan', key: 'pengaturan', icon: <Settings size={20} /> },
 ];
 
 export const MENU_ITEMS_MAHASISWA = [
   { name: 'Beranda', key: 'Beranda', icon: <Home size={20} /> },
   {
     name: 'Data Pelanggaran',
-    key: 'Data Pelanggaran',
+    key: 'datapelanggaran',
     icon: <FileX size={20} />,
   },
   {
     name: 'Data Pembayaran',
-    key: 'Data Pembayaran',
+    key: 'datapembayaran',
     icon: <CreditCard size={20} />,
   },
-  { name: 'Pengaduan', key: 'Pengaduan', icon: <Megaphone size={20} /> },
+  { name: 'Pengaduan', key: 'pengaduan', icon: <Megaphone size={20} /> },
   {
     name: 'Jadwal Kegiatan',
-    key: 'Jadwal Kegiatan',
+    key: 'jadwalkegiatan',
     icon: <CalendarClock size={20} />,
   },
-  { name: 'Pengaturan', key: 'Pengaturan', icon: <Settings size={20} /> },
+  { name: 'Pengaturan', key: 'pengaturan', icon: <Settings size={20} /> },
 ];
