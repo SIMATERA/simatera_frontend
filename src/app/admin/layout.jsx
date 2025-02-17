9b7661f492dc873f83692b860fe5b5b804517877
josray10 <jossyraya.saragih@gmail.com>
2025-02-18 02:29:28 +0700

    style: update AdminLayout component styles for improved layout and overflow handling

--- src/app/admin/layout.jsx
+++ src/app/admin/layout.jsx
@@ -3,16 +3,15 @@ import Sidebar from '@/components/sidebar';
 
 export default function AdminLayout({ children, params }) {
   // Misal role di sini sudah diketahui dari AuthContext
-  // Untuk contoh, kita anggap role = 'admin'
   const role = 'admin';
 
   return (
-    <div className="flex flex-row bg-[#EEECEC]">
-      <header className="flex  h-screen">
+    <div className="flex flex-row bg-[#F5F6FA] overflow-x-hidden">
+      <header className="flex h-screen">
         <Sidebar role={role} />
       </header>
-      <main className="flex  w-screen ">
-        <div className="w-full flex flex-col h-screen scrollbar-hide overflow-y-auto gap-5">
+      <main className="flex w-full">
+        <div className="w-full flex flex-col h-screen overflow-y-auto scrollbar-hide gap-5">
           {children}
         </div>
       </main>
